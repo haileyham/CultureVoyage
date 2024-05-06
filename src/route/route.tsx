f8b2af25ac77ab6c5056c2804918f37af1a8a37e
hailey ham <roserealslow@gmail.com>
2024-05-06 16:50:43 +0900

    파일삭제 및 경로 변경

--- src/route/route.tsx
+++ src/route/route.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from '../pages/main/Main';
 import Searching from '../pages/search/Search';
-import List from '../pages/list/List';
 
 export default function route() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/list" element={<List />} />
-                <Route path="/search" element={<Searching />} />
+                <Route path="/" element={<Searching />} />
             </Routes>
         </BrowserRouter>
     );
